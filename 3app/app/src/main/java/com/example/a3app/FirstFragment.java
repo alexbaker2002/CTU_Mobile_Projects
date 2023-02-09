b7fb08ca33b427d752468070f4a74189eeace01e
Alex Baker <alex_baker2002@hotmail.com>
2023-02-08 21:07:25 -0600

    finish assignment 3

--- 3app/app/src/main/java/com/example/a3app/FirstFragment.java
+++ 3app/app/src/main/java/com/example/a3app/FirstFragment.java
@@ -12,6 +12,9 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.a3app.databinding.FragmentFirstBinding;
 import android.widget.EditText;
 
+
+import java.util.ArrayList;
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
@@ -48,8 +51,63 @@ public class FirstFragment extends Fragment {
 
             }
         });
-    }
+        binding.buttonFirst2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                EditText fName = getView().findViewById(R.id.editTextTextPersonName);
+                firstName = fName.getText().toString().trim();
+                EditText lName = getView().findViewById(R.id.editTextTextPersonName2);
+                lastName = lName.getText().toString().trim();
+                EditText output = getView().findViewById(R.id.editTextTextPersonName3);
+                if(firstName.equals(lastName)){
+                    output.setText("Strings are Equal");
+                }else{
+                    output.setText("Strings are not Equal");
+                }
+
+
+            }
+        });
+        binding.buttonFirst3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText fName = getView().findViewById(R.id.editTextTextPersonName);
+                firstName = fName.getText().toString().trim().toLowerCase();
+                EditText lName = getView().findViewById(R.id.editTextTextPersonName2);
+                lastName = lName.getText().toString().trim().toLowerCase();
+                EditText output = getView().findViewById(R.id.editTextTextPersonName3);
+                int count = 0;
+                count = count + CountVowels(firstName);
+                count = count + CountVowels(lastName);
+
+
+                if (count > 0) {
+
+                    output.setText(count + " vowels found in both inputs.");
+                }else {
+                    output.setText("No vowels found in either input.");
+                }
+
+            }
+        });
+
+    }
+    private int CountVowels(String str){
+        System.out.println(str);
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        int count = 0;
+        for (char ch: str.toCharArray()) {
+            System.out.println(ch);
+            if(new String(vowels).indexOf(ch) > -1){
+                System.out.println("added 1");
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
